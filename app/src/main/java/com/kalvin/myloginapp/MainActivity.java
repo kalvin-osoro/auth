34158b01827ef45f283430e5c54a2952181d7528
kalvin-osoro <62253122+kalvin-osoro@users.noreply.github.com>
2022-07-03 23:08:30 +0300

    second commit

---
+++ app/src/main/java/com/kalvin/myloginapp/MainActivity.java
@@ -0,0 +1,36 @@
+package com.kalvin.myloginapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView register;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        register = (TextView) findViewById(R.id.gotoreg);
+        register.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.gotoreg:
+                startActivity(new Intent(this,RegisterUser.class));
+                break;
+
+        }
+    }
+}
